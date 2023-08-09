173cdb116a89ee08be4e61c878ee1185d5576597
Bellringer5984 <cjbellmyer5984@gmail.com>
2023-08-09 12:27:46 -0500

    slight bug in board. the cards aren't holding position

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,9 +5,11 @@ import { useBoardStore } from '@/store/BoardStore'
 import Column from './Column'
 
 function Board() {
-  const [board, getBoard] = useBoardStore((state)  => [
+  const [board, getBoard, setBoardState] = useBoardStore((state)  => [
     state.board,
-    state.getBoard])
+    state.getBoard,
+    state.setBoardState,
+  ])
 
 
   useEffect(() => {
@@ -15,8 +17,62 @@ function Board() {
   },[getBoard])
 
   const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type} = result
+  
+      //handle if there is no destination to drop in
+      if (!destination) return;
 
-  } 
+      //handle column drag
+      if (type === "column") {
+        const entries = Array.from(board.columns.entries());
+        const [removed] = entries.splice(source.index, 1);
+        entries.splice(destination.index, 0, removed);
+        const rearrangedColumns = new Map(entries);
+        setBoardState ({
+          ...board,
+          columns: rearrangedColumns,
+        })
+      
+        //this step is needed as the indexes are stored as numbers 0,1,2 etc. instead of id's with DND library
+        const columns = Array.from(board.columns);
+        const startColIndex = columns[Number(source.droppableId)]
+        const finishColIndex = columns[Number(destination.droppableId)]
+
+        const startCol: Column = {
+          id: startColIndex[0],
+          todos: startColIndex[1].todos,
+        };
+
+        const finishCol: Column = {
+          id: finishColIndex[0],
+          todos: finishColIndex[1].todos
+        }
+      
+
+        if (!startCol || !finishCol) return;
+
+        if (source.index === destination.index && startCol === finishCol) return;
+
+        const newTodos = startCol.todos;
+        const [todoMoved] = newTodos.splice(source.index, 1);
+
+        if (startCol.id === finishCol.id) {
+          //Same Column task drag
+          newTodos.splice(destination.index, 0, todoMoved);
+          const newCol = {
+            id: startCol.id,
+            todos: newTodos,
+          };
+          const newColumns = new Map(board.columns);
+          newColumns.set(startCol.id, newCol);
+
+          setBoardState({...board, columns: newColumns })
+
+
+        } else {
+          //Dragging to another column
+        }
+    } 
 
   console.log(board)
   return (
