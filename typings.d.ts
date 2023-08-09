76490a26525e894fb8275de834cb54c20784adfe
Bellringer5984 <cjbellmyer5984@gmail.com>
2023-08-09 10:42:36 -0500

    added todo titles and x button

---
+++ typings.d.ts
@@ -0,0 +1,27 @@
+import { Models } from "appwrite";
+
+interface Board {
+  column: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inprogress" | "done";
+
+interface Column {
+  id: TypedColumn;
+  todos: Todo[];
+}
+
+interface Todo extends Models.Document{
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: TypedColumn;
+  image?: Image;
+}
+
+interface Image {
+  bucketId: string;
+  fileId: string;
+}
+
+
