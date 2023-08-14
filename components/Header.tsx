a773941a8f55809c9d87f67589502c680e568a6d
Bellringer5984 <cjbellmyer5984@gmail.com>
2023-08-14 12:21:23 -0500

    formatted data to send to ai

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,37 @@
-'use client'
+"use client";
 
-import Image from  "next/image"
-import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
-import Avatar from "react-avatar"
-import { useBoardStore } from "@/store/BoardStore"
+import Image from "next/image";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect, useState } from "react";
 
 function Header() {
-    const [searchString, setSearchString] = useBoardStore((state) => [
-        state.searchString,
-        state.setSearchString,
-    ])
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
+    state.searchString,
+    state.setSearchString,
+  ]);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("")
+
+  useEffect;(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+        const suggestion = await fetchSuggestionFunc(board);
+        setSuggestion(suggestion);
+        setLoading(false);
+    }
+  }, [board])
 
   return (
-  <header>
-        <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
-
-            <div className="
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div
+          className="
             absolute 
             top-0
             left-0
@@ -30,38 +46,43 @@ function Header() {
             opacity-50
             -z-50
             "
+        />
+
+        <Image
+          src="http://links.papareact.com/c2cdd5"
+          alt="Trello Logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+          {/* Search Box */}
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+              className="flex-1 outline-none p-2"
             />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
 
-           
-
-
-            <Image 
-                src="http://links.papareact.com/c2cdd5"
-                alt="Trello Logo"
-                width={300}
-                height={100}
-                className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
-            />
-
-            <div className="flex items-center space-x-5 flex-1 justify-end w-full">
-                {/* Search Box */}
-                <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
-                    <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
-                    <input type="text" placeholder="Search" value={searchString} onChange={(e) => setSearchString(e.target.value)} className="flex-1 outline-none p-2"/>
-                    <button type="submit" hidden>Search</button>
-                </form>
-
-                <Avatar name="Caleb Bellmyer" round size="50" color="#0055D1"  />
-            </div>
+          <Avatar name="Caleb Bellmyer" round size="50" color="#0055D1" />
         </div>
-        <div className="flex items-center justify-center px-5 md:py-5">
-            <p className="flex items-center px-5 py-2 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-                <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />    
-                GPT is summarising you task for the day... 
-            </p>
-        </div>  
+      </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center px-5 py-2 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is summarising you task for the day...
+        </p>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
